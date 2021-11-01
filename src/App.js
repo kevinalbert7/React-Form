e2b8cbaa79b6f3108c35064d803b36d73c6ad94f
kevinalbert7 <20ko@live.fr>
2021-11-01 21:02:53 +0100

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor () {
+    super () 
+  
+    this.state = {
+      email: "",
+      password: "",
+      rememberMe: false,
+      emailIsValid: false,
+      passwordIsValid: false,
+      isSubmitted: false
+    }
+
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleRememberMeChange = this.handleRememberMeChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    test() {
+      
+    }
+
+    handleEmailChange(e) {
+      console.log(e.target.value)
+      this.setState({ email: e.target.value })
+    }
+
+    handlePasswordChange(e) {
+      console.log(e.target.value)
+      this.setState({ password: e.target.value })
+    }
+
+    handleRememberMeChange(e) {
+      this.setState({ rememberMe: e.target.value })
+    }
+
+    handleSubmit(e) {
+      e.preventDefault()
+      this.setState({ submit: e.target.value })
+      }
+
+
+    render() {
+      return (
+        
+          <div>
+            <form>
+              <div className="mb-3">
+                <label className="form-label">Email address</label>
+                <input 
+                type="email" 
+                className="form-control" 
+                placeholder="Enter email..."
+                onFocus=""
+                onChange={this.handleEmailChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input 
+                type="password" 
+                className="form-control" 
+                placeholder="Enter password..."
+                onFocus=""
+                onChange={this.handlePasswordChange}
+                />
+
+              </div>
+              <div className="mb-3 form-check">
+                <input 
+                type="checkbox" 
+                className="form-check-input"
+                />
+                <label className="form-check-label">Remember Me</label>
+              </div>
+                <button 
+                type="submit" 
+                className="btn btn-primary"
+                onChange={this.handleSubmit}
+                >Submit</button>
+            </form>
+          </div>
+        
+      );
+    }
 }
 
-export default App;
+export default App
